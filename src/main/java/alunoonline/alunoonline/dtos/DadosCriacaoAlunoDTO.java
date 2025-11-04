07c3301eab03f19ef24b57794cf4a2a284a18d0d
Samuel Lorand <samuel.lorand@hotmail.com>
2025-11-04 14:31:08 -0300

    Finalizado com atualizacao para banco MySQL

--- src/main/java/alunoonline/alunoonline/dtos/DadosCriacaoAlunoDTO.java
+++ src/main/java/alunoonline/alunoonline/dtos/DadosCriacaoAlunoDTO.java
@@ -1,10 +1,9 @@
 package alunoonline.alunoonline.dtos;
 
-// Usar um record é a forma mais moderna e simples
 public record DadosCriacaoAlunoDTO(
         String nome,
         String cpf,
         String email,
-        String senha // <-- AQUI ESTÁ A NOVIDADE!
+        String senha
 ) {
 }
